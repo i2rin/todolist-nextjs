db8bceb1e03851ffd739b3d0f20803119863d699
i2rin <tangairin3@gmail.com>
2024-01-18 18:22:35 +0900

    ✨日付追加機能を実装しました

--- src/app/components/AddTask.tsx
+++ src/app/components/AddTask.tsx
@@ -5,11 +5,14 @@ import {v4 as uuidv4} from "uuid";
 
 const AddTask = () => {
     const [taskTitle, setTaskTitle] = useState("");
+    const [deadline, setDeadline] = useState("");
     
     const handleSubmit = async (e:FormEvent) => {
         e.preventDefault();
-        await addTodo({ id: uuidv4(), text: taskTitle  });
+        const deadlineDate = new Date(deadline);
+        await addTodo({ id: uuidv4(), text: taskTitle, date: deadlineDate });
         setTaskTitle("");
+        setDeadline("");
     };
 
   return (
@@ -20,6 +23,12 @@ const AddTask = () => {
             onChange = {(e: ChangeEvent<HTMLInputElement>) => setTaskTitle(e.target.value)}
             value = {taskTitle}
          />
+        <input 
+            type = "date"
+            value = {deadline}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => setDeadline(e.target.value)}
+            className = "w-full border px-4 py-2 rounded-lg focus:ourline-none fucus:boder-blue-400"
+        />
         <button className="w-full px-4 py-2 text-white bg-blue-500 rounded transform hover:bg-blue-400 hover:scale-95 duration-200">Add Task</button>
     </form>
   )  
